8b11d1cac453345b6a0f03cfa9b0fde96366aa47
prabeera-Dixit <75612017+prabeera-Dixit@users.noreply.github.com>
2022-06-22 16:52:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAEmag1aWhWFQfVvho1RalvmrpWGZ-r4V0",
+  authDomain: "study-guide-app-d0c57.firebaseapp.com",
+  databaseURL: "https://study-guide-app-d0c57-default-rtdb.firebaseio.com",
+  projectId: "study-guide-app-d0c57",
+  storageBucket: "study-guide-app-d0c57.appspot.com",
+  messagingSenderId: "427867986191",
+  appId: "1:427867986191:web:594021d9b15d999972867a"
+};
